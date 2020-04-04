04170df5043c370419913f341a8c41b00d460693
Emilio <emilio.2rj@gmail.com>
2020-04-04 02:16:05 -0700

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    urlDB ='mongodb+srv://emilio:nNXkfqeXzGo3JoZ9@cluster0-d0lxm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
